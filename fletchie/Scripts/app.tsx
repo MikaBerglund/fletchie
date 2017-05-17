deafd2d5819aafadd9aa861938e202411e39e7be
Mika Berglund <mika.berglund@iki.fi>
2017-05-17 12:31:05 +0300

    -

---
+++ fletchie/Scripts/app.tsx
@@ -0,0 +1,7 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as odata from "./modules/odata/OData";
+
+odata.queryBuilder("http://services.odata.org/V3/OData/OData.svc/Products")
+    ;
+
--- fletchie/wwwroot/js/build/app.js
+++ fletchie/wwwroot/js/build/app.js
@@ -1,14 +1,6 @@
 define(["require", "exports", "./modules/odata/OData"], function (require, exports, odata) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    odata.queryBuilder("/api/Activities")
-        .filterString("Title", odata.ComparisonOperator.contains, "call")
-        .inlineCount(odata.InlineCountMode.AllPages)
-        .skip(0)
-        .top(20)
-        .expand("Owner")
-        .expand("Tenant")
-        .orderBy("Date desc")
-        .orderBy("Created asc");
+    odata.queryBuilder("http://services.odata.org/V3/OData/OData.svc/Products");
 });
 //# sourceMappingURL=app.js.map
